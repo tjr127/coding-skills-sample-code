df95519c2139707a6e714a2034c71371a37ad5c4
tjr127 <ty.rost@rostlabs.com>
2020-03-25 13:27:56 -0400

    get-ap-xml-3.py

--- coding201-parsing-xml/get-ap-xml-3.py
+++ coding201-parsing-xml/get-ap-xml-3.py
@@ -1,13 +1,17 @@
 from urllib.request import Request, urlopen
-import xml.dom.minidom 
+#import xml.dom.minidom 
+from xml.dom.minidom import parseString
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 req = Request('https://msesandbox.cisco.com/api/contextaware/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
 req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc==')
 response = urlopen(req)
 responseString = response.read().decode("utf-8")
-dom = xml.dom.minidom.parseString(responseString)
+dom = parseString(responseString)
 xml = dom.toprettyxml()
 print(xml)
 floor_element = dom.firstChild
+print(floor_element)
 if floor_element.hasChildNodes :
  child = floor_element.firstChild
  while child is not None:
