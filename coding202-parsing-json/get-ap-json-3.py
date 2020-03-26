0f3baaeace447a81a547b6726c5ca05de64e1244
tjr127 <ty.rost@rostlabs.com>
2020-03-25 20:44:44 -0400

    parsing json

--- coding202-parsing-json/get-ap-json-3.py
+++ coding202-parsing-json/get-ap-json-3.py
@@ -1,12 +1,16 @@
 from urllib.request import Request, urlopen
 import json
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-req = Request('https://devnetapi.cisco.com/sandbox/mse/api/config/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
+req = Request('https://cmxlocationsandbox.cisco.com/api/config/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
+
+#req = Request('https://devnetapi.cisco.com/sandbox/mse/api/config/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
 req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc=')
 req.add_header('Accept', 'application/json')
 response = urlopen(req)
 response_string = response.read().decode("utf-8")
-# print(responseString)
+print(response_string)
 json_object = json.loads(response_string)
 print(json_object)
 print(json.dumps(json_object, sort_keys=True, indent=4))
